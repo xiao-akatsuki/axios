b01d9b4a00bf4c2e2e9a098c7f942ec8b1e2e668
xiaoxunyao <huchengyea@163.com>
2021-11-17 15:27:49 +0800

    add default ssl

---
+++ src/main/java/com/axios/core/config/ssl/DefaultSSLFactory.java
@@ -0,0 +1,16 @@
+package com.axios.core.config.ssl;
+
+/**
+ * [默认的SSLSocketFactory](Default sslsocketfactory)
+ * @description zh - 默认的SSLSocketFactory
+ * @description en - Default sslsocketfactory
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-11-17 13:54:38
+ */
+public class DefaultSSLFactory extends CustomProtocolsSSLFactory {
+
+	public DefaultSSLFactory() {
+	}
+
+}
