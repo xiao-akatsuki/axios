87c13b484a42b23690be499133dbb957904b6857
xiaoxunyao <huchengyea@163.com>
2021-11-17 15:27:13 +0800

    add assertion

--- src/main/java/com/axios/core/assertion/Assert.java
+++ src/main/java/com/axios/core/assertion/Assert.java
@@ -12,6 +12,81 @@ import java.util.function.Supplier;
  */
 public class Assert {
 
+	/**
+	 * [断言对象是否不为 null](Asserts whether the object is not null)
+	 * @description zh - 断言对象是否不为 null
+	 * @description en - Asserts whether the object is not null
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-11-17 15:02:27
+	 * @param object 判断的对象
+	 * @param errorSupplier 错误抛出异常附带的消息生产接口
+	 * @return T
+	 */
+	public static <T, X extends Throwable> T notNull(T object, Supplier<X> errorSupplier) throws X {
+		if (null == object) {
+			throw errorSupplier.get();
+		}
+		return object;
+	}
+
+	/**
+	 * [断言对象是否不为 null](Asserts whether the object is not null)
+	 * @description zh - 断言对象是否不为 null
+	 * @description en - Asserts whether the object is not null
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-11-17 15:04:04
+	 * @param object 对象
+	 * @param errorMssage 错误信息
+	 * @throws java.lang.IllegalArgumentException
+	 * @return T
+	 */
+	public static <T> T notNull(T object, String errorMssage) throws IllegalArgumentException {
+		return notNull(object, () -> new IllegalArgumentException(errorMssage));
+	}
+
+	/**
+	 * [断言对象是否不为 null](Asserts whether the object is not null)
+	 * @description zh - 断言对象是否不为 null
+	 * @description en - Asserts whether the object is not null
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-11-17 15:04:49
+	 * @param object 对象
+	 * @throws java.lang.IllegalArgumentException
+	 * @return T
+	 */
+	public static <T> T notNull(T object) throws IllegalArgumentException {
+		return notNull(object, "[Assertion failed] - this argument is required; it must not be null");
+	}
+
+	/**
+	 * [检查给定字符串是否为空白](Checks whether the given string is blank)
+	 * @description zh - 检查给定字符串是否为空白
+	 * @description en - Checks whether the given string is blank
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-11-17 15:05:06
+	 * @param text 对象
+	 * @throws java.lang.IllegalArgumentException
+	 * @return T
+	 */
+	public static <T extends CharSequence> T notBlank(T text) throws IllegalArgumentException {
+		return notBlank(text, "[Assertion failed] - this String argument must have text; it must not be null, empty, or blank");
+	}
+
+	/**
+	 * [检查给定字符串是否为空白](Checks whether the given string is blank)
+	 * @description zh - 检查给定字符串是否为空白
+	 * @description en - Checks whether the given string is blank
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-11-17 14:50:00
+	 * @param text 判断的文本
+	 * @param errorMessage 错误信息
+	 * @return T
+	 */
 	public static <T extends CharSequence> T notBlank(T text, String errorMessage) throws IllegalArgumentException {
 		return notBlank(text, () -> new IllegalArgumentException(errorMessage));
 	}
