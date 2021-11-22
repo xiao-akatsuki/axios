1047344c9eb380be4709a1dd4828d6a58e52df54
xiaoxunyao <huchengyea@163.com>
2021-11-22 21:22:22 +0800

    add matcher

---
+++ src/main/java/com/axios/core/matcher/Matcher.java
@@ -0,0 +1,17 @@
+package com.axios.core.matcher;
+/**
+ * 匹配接口
+ *
+ * @param <T> 匹配的对象类型
+ * @author Looly
+ */
+@FunctionalInterface
+public interface Matcher<T> {
+	/**
+	 * 给定对象是否匹配
+	 *
+	 * @param t 对象
+	 * @return 是否匹配
+	 */
+	boolean match(T t);
+}
