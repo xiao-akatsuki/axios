272a2510f0795964a9e3502e0e86bb560f47ec28
xiaoxunyao <huchengyea@163.com>
2021-11-24 20:36:49 +0800

    init http request

--- src/main/java/com/axios/core/http/HttpRequest.java
+++ src/main/java/com/axios/core/http/HttpRequest.java
@@ -5,6 +5,7 @@ import javax.net.ssl.SSLSocketFactory;
 
 import com.axios.core.config.global.GlobalCookieManager;
 import com.axios.core.config.global.HttpGlobalConfig;
+import com.axios.core.connection.Connection;
 import com.axios.core.http.url.UrlBuilder;
 import com.axios.core.requestMethod.RequestMethod;
 
@@ -34,10 +35,89 @@ public class HttpRequest extends HttpBase<HttpRequest> {
 
 	/** URL */
 	private UrlBuilder url;
+
 	/** header */
 	private URLStreamHandler urlHandler;
+
 	/** method */
 	private RequestMethod method = RequestMethod.GET;
+
+	/**
+	 * 请求前的拦截器，用于在请求前重新编辑请求
+	 */
+	private final HttpInterceptor.Interceptor interceptors = new HttpInterceptor.Interceptor();
+
+	/**
+	 * 默认连接超时
+	 */
+	private int connectionTimeout = HttpGlobalConfig.timeout;
+
+	/**
+	 * 默认读取超时
+	 */
+	private int readTimeout = HttpGlobalConfig.timeout;
+
+	/**
+	 * 存储表单数据
+	 */
+	private Map<String, Object> form;
+
+	/**
+	 * 是否为Multipart表单
+	 */
+	private boolean isMultiPart;
+
+	/**
+	 * Cookie
+	 */
+	private String cookie;
+
+	/**
+	 * 连接对象
+	 */
+	private Connection httpConnection;
+	/**
+	 * 是否禁用缓存
+	 */
+	private boolean isDisableCache;
+	/**
+	 * 是否是REST请求模式
+	 */
+	private boolean isRest;
+	/**
+	 * 重定向次数计数器，内部使用
+	 */
+	private int redirectCount;
+	/**
+	 * 最大重定向次数
+	 */
+	private int maxRedirectCount;
+	/**
+	 * Chuncked块大小，0或小于0表示不设置Chuncked模式
+	 */
+	private int blockSize;
+	/**
+	 * 代理
+	 */
+	private Proxy proxy;
+
+	/**
+	 * HostnameVerifier，用于HTTPS安全连接
+	 */
+	private HostnameVerifier hostnameVerifier;
+	/**
+	 * SSLSocketFactory，用于HTTPS安全连接
+	 */
+	private SSLSocketFactory ssf;
+
+	public HttpRequest(String url) {
+		this(UrlBuilder.ofHttp(url));
+	}
+
+	public HttpRequest(UrlBuilder url) {
+		this.url = url;
+		this.header(GlobalHeaders.INSTANCE.headers);
+	}
 
 	/**
 	 * [设置全局默认的连接和读取超时时长](Set the global default connection and read timeout)
@@ -90,9 +170,6 @@ public class HttpRequest extends HttpBase<HttpRequest> {
 		GlobalCookieManager.setCookieManager(null);
 	}
 
-	/**
-	 * 请求前的拦截器，用于在请求前重新编辑请求
-	 */
-	private final HttpInterceptor.Chain interceptors = new HttpInterceptor.Chain();
+
 
 }
