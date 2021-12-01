7bfd1653fab59635a7458ffd283ea2e46a0482c3
xiaoxunyao <huchengyea@163.com>
2021-12-01 22:15:55 +0800

    init http response body

--- src/main/java/com/axios/response/HttpResponse.java
+++ src/main/java/com/axios/response/HttpResponse.java
@@ -13,10 +13,12 @@ import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map.Entry;
 
+import com.axios.core.config.global.GlobalCookieManager;
 import com.axios.core.connection.Connection;
 import com.axios.core.http.HttpBase;
 import com.axios.core.tool.UrlTool;
 import com.axios.core.tool.http.HttpTool;
+import com.axios.exception.HttpException;
 import com.axios.header.RequestHeader;
 
 /**
@@ -118,8 +120,17 @@ public class HttpResponse extends HttpBase<HttpResponse> implements Closeable {
 		return header(RequestHeader.CONTENT_ENCODING);
 	}
 
+	/**
+	 * [获取内容编码](Get content encoding)
+	 * @description zh - 获取内容编码
+	 * @description en - Get content encoding
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:07:19
+	 * @return long
+	 */
 	public long contentLength() {
-		long contentLength = HttpTool.toLong(header(RequestHeader.CONTENT_LENGTH), -1L);
+		long contentLength = Long.valueOf(header(RequestHeader.CONTENT_LENGTH));
 		if (contentLength > 0 && (isChunked() || UrlTool.isNotBlank(contentEncoding()))) {
 			contentLength = -1;
 		}
@@ -139,5 +150,142 @@ public class HttpResponse extends HttpBase<HttpResponse> implements Closeable {
 		final String contentEncoding = contentEncoding();
 		return "gzip".equalsIgnoreCase(contentEncoding);
 	}
+
+	/**
+	 * [是否为zlib(Deflate)压缩过的内容](Is it zlib (deflate) compressed content)
+	 * @description zh - 是否为zlib(Deflate)压缩过的内容
+	 * @description en - Is it zlib (deflate) compressed content
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 21:57:44
+	 * @return boolean
+	 */
+	public boolean isDeflate() {
+		final String contentEncoding = contentEncoding();
+		return "deflate".equalsIgnoreCase(contentEncoding);
+	}
+
+	/**
+	 * [是否为Transfer-Encoding:Chunked的内容](Is it the content of transfer encoding: chunked)
+	 * @description zh - 是否为Transfer-Encoding:Chunked的内容
+	 * @description en - Is it the content of transfer encoding: chunked
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 21:59:34
+	 * @return boolean
+	 */
+	public boolean isChunked() {
+		final String transferEncoding = header(RequestHeader.TRANSFER_ENCODING);
+		return "Chunked".equalsIgnoreCase(transferEncoding);
+	}
+
+	/**
+	 * [获取本次请求服务器返回的Cookie信息](Obtain the cookie information returned by the server for this request)
+	 * @description zh - 获取本次请求服务器返回的Cookie信息
+	 * @description en - Obtain the cookie information returned by the server for this request
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:00:10
+	 * @return java.lang.String
+	 */
+	public String getCookieStr() {
+		return header(RequestHeader.SET_COOKIE);
+	}
+
+	/**
+	 * [获取Cookie](get Cookie)
+	 * @description zh - 获取Cookie
+	 * @description en - get Cookie
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:00:56
+	 */
+	public List<HttpCookie> getCookies() {
+		return GlobalCookieManager.getCookies(this.httpConnection);
+	}
+
+	/**
+	 * [获取Cookie](get Cookie)
+	 * @description zh - 获取Cookie
+	 * @description en - get Cookie
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:03:37
+	 * @param name cookie名字
+	 * @return java.net.HttpCookie
+	 */
+	public HttpCookie getCookie(String name) {
+		List<HttpCookie> cookie = getCookies();
+		if (null != cookie) {
+			for (HttpCookie httpCookie : cookie) {
+				if (httpCookie.getName().equals(name)) {
+					return httpCookie;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * [获取Cookie值](get cookie value)
+	 * @description zh - 获取Cookie值
+	 * @description en - get cookie value
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:04:34
+	 * @param name cookie名字
+	 * @return java.lang.String
+	 */
+	public String getCookieValue(String name) {
+		HttpCookie cookie = getCookie(name);
+		return (null == cookie) ? null : cookie.getValue();
+	}
+
+	/** ---------------- response body ---------------- */
+
+	/**
+	 * [获得服务区响应流](Get service area response flow)
+	 * @description zh - 获得服务区响应流
+	 * @description en - Get service area response flow
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:09:17
+	 * @return java.io.InputStream
+	 */
+	public InputStream bodyStream() {
+		return isAsync ?
+			this.in :
+			new ByteArrayInputStream(this.bodyBytes);
+	}
+
+	/**
+	 * [获取响应流字节码](Get response stream bytecode)
+	 * @description zh - 获取响应流字节码
+	 * @description en - Get response stream bytecode
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:10:08
+	 * @return byte[]
+	 */
+	public byte[] bodyBytes() {
+		sync();
+		return this.bodyBytes;
+	}
+
+	/**
+	 * [获取响应主体](Get response body)
+	 * @description zh - 获取响应主体
+	 * @description en - Get response body
+	 * @version V1.0
+	 * @author XiaoXunYao
+	 * @since 2021-12-01 22:13:04
+	 * @throws com.axios.exception.HttpException
+	 * @return java.lang.String
+	 */
+	public String body() throws HttpException {
+		return HttpTool.getString(bodyBytes(), this.charset, null == this.charsetFromResponse);
+	}
+
+
 
 }
