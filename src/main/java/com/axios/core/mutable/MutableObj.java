0fd7f5af174b97e253bdc5b554cf83ed2d6fcf91
xiaoxunyao <huchengyea@163.com>
2021-12-05 09:58:14 +0800

    add mutable object

---
+++ src/main/java/com/axios/core/mutable/MutableObj.java
@@ -0,0 +1,59 @@
+package com.axios.core.mutable;
+
+import java.io.Serializable;
+
+/**
+ * [可变 Object](Variable object)
+ * @description zh - 可变 Object
+ * @description en - Variable object
+ * @version V1.0
+ * @author XiaoXunYao
+ * @since 2021-12-05 09:40:51
+ */
+public class MutableObj<T> implements Mutable<T>, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private T value;
+
+	public MutableObj() {
+	}
+
+	public MutableObj(final T value) {
+		this.value = value;
+	}
+
+	@Override
+	public T get() {
+		return this.value;
+	}
+
+	@Override
+	public void set(final T value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		if (this.getClass() == obj.getClass()) {
+			final MutableObj<?> that = (MutableObj<?>) obj;
+			return this.value.equals(that.value);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return value == null ? 0 : value.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return value == null ? "null" : value.toString();
+	}
+}
